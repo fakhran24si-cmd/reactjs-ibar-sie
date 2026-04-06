2fd1b237e1e95934dad901c5eb41667e6056b353
fakhran ikbaar <fakhran24si@mahasiswa.pcr.ac.id>
2026-04-06 16:26:53 +0700

    pertemuan-4 (yang benar, bukan yang tugas sebelumnya)

--- src/pertemuan-4/Main.jsx
+++ src/pertemuan-4/Main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './pertemuan-4/App.css'
-import App from './pertemuan-4/App.jsx'
+import { createRoot } from "react-dom/client";
+import "./tailwind.css";
+import FrameworkList from "./FrameworkListSearcher";
+import ResponsiveText from "./ResponsiveText";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+createRoot(document.getElementById("root"))
+    .render(
+        <div>
+            {/* <FrameworkList/>         */}
+            <ResponsiveText/>
+        </div>
+        
+    )
